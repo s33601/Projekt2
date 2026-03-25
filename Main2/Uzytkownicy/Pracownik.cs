c5b39e81cfd81f4824751b282a7829f7dfc25233
Adam Wyrzykowski <s33601@pjwstk.edu.pl>
2026-03-25 11:04:33 +0100

    Dodanie klas student i pracownik

---
+++ Main2/Uzytkownicy/Pracownik.cs
@@ -0,0 +1,6 @@
+﻿namespace Main2.Uzytkownicy;
+
+public class Pracownik : Osoba
+{
+    public override int limitWypozyczenia { get; } = 5;
+}
---
+++ Main2/Uzytkownicy/Student.cs
@@ -0,0 +1,6 @@
+﻿namespace Main2.Uzytkownicy;
+
+public class Student : Osoba
+{
+    public override int limitWypozyczenia { get; } = 2;
+}
