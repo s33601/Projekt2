a7873baf875ac0da65340eb1acb32f2ad09e9ffb
Adam Wyrzykowski <s33601@pjwstk.edu.pl>
2026-03-25 11:05:56 +0100

    Dodanie klasy Wypozyczalnia do sledzenia historii

---
+++ Main2/Transakcje/Wypozyczalnia.cs
@@ -0,0 +1,16 @@
+﻿using Main2.Sprzety;
+using Main2.Uzytkownicy;
+
+namespace Main2.Transakcje;
+
+public class Wypozyczenia
+{
+    public Osoba osoba{set;get;}
+    public Sprzet sprzet{set;get;}
+    public DateTime odkiedy { get; set; }
+    public DateTime doKiedy { get; set; }
+    public DateTime? kiedyOddany { get; set; }
+    public double kwotaKary { get; set; }
+    
+    public bool czyZwrotTerminowosc { get; set; }
+}
