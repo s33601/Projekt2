52ac5d4ce467cef4b76a79328f8578e53f089ce3
Adam Wyrzykowski <s33601@pjwstk.edu.pl>
2026-03-25 11:49:29 +0100

    Dodanie podstawowych metod

---
+++ Main2/ZarzadzeniaWypozyczalnia.cs
@@ -0,0 +1,24 @@
+﻿using Main2.Sprzety;
+using Main2.Transakcje;
+using Main2.Uzytkownicy;
+
+namespace Main2;
+
+public class ZarzadzeniaWypozyczalnia
+{
+    private List<Sprzet> listaSprzetu = new List<Sprzet>();
+    private List<Osoba> listaOsob = new List<Osoba>();
+    private List<Wypozyczenia> listaWypozyczen = new List<Wypozyczenia>();
+
+    public void dodawnieSprzetu(Sprzet sprzet)
+    {
+        sprzet.ID = listaSprzetu.Count + 1;
+        listaSprzetu.Add(sprzet);
+    }
+
+    public void dodawnieOsoba(Osoba osoba)
+    {
+        osoba.ID = listaOsob.Count + 1;
+        listaOsob.Add(osoba);
+    }
+}
